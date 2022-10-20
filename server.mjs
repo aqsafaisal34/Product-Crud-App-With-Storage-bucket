9c3fd9fb536621f0fd5c2731d370fd7a1d3d3e97
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-10-20 21:27:09 +0500

    work done

--- server.mjs
+++ server.mjs
@@ -1,205 +1,187 @@
-import express, { request } from "express"
-import cors from "cors"
-import mongoose from 'mongoose';
-import fs from 'fs';
+import express, { request } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import fs from "fs";
 import admin from "firebase-admin";
 import { stringToHash } from "bcrypt-inzi";
 
-https://firebase.google.com/docs/storage/admin/start
-var serviceAccount ={
-  "type": "service_account",
-  "project_id": "productcrudapp",
-  "private_key_id": "3d8bfb1bc1b42217c4bff649e90cf2d8f55f4354",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCOHQnpfxU6HwIY\nH5DEjQwHZSxJm03DdgFTLD0Ns6wF7XOdpTv3UFuLc//LgToANg6zSgk44Ha3URxz\nXPrZA6/qVaA5nbgK6/LTiaQq5gZu60H9YsRaMqQutt/O5vvHn12cFhWog6V7EFIB\nNG+64b3kwEO/1aghgRYfGV103tzyjncnxJTUkCePcetvK9qjpWsY+tybRuNX0WkO\nzdxrCakdL80HwL73mj73c0AC9CH5zDq6mzrV+DlN8TN6m/CJegky1z8RLsXg0te0\nKw+/JElKAjpur+2HjRzKxtj2K2v23ewRkZ9QtDV5xwJBg2dMR/ycljCl6WLn+E+Z\nC1Ghotf/AgMBAAECggEAA46th1dDiENrcYAkxw7CzNKXA7asAScMjHDwjE3Bkhpq\nCPmwYRX1XK3IKudutL+lqY5CYdaYkikpHagnrVP+zM5c5uRQTZGvb+Ujo2FDqvYf\nnkChKqLW4kZ4nrzAc7RFCxmwbBnzg/uaGeaen150OuwnJf+qqEALdjbT1I+Zl0a5\nHTpKYm1IXzQkGW9B3aXylnGmR6cKWoIlmceGXG6DFDDzXgQqJCxsNQLftkT0Nq+V\nYw4ucP0oqJrkzOKsk5IZwWOeAkzC21v/Jux+nZ0w3FgyDFAIQsN/oPf0u4xAIuRH\nHWmyaZ27gh7fQK9aRso8Golyiz1zqzDTJf5Zwxk0FQKBgQDGsqc1KeejOX1zoi4E\nsKszlHJggGeJefwqt70NvMb+TkTxDJs/wNQ/92bnBwagQkdf93ObErucZWNlAZnt\n3CrPwAOQvO9POsfpBj4dnl7uLYAw67bULV4RmZMpUAPO8pU3IsnGt/L6sZ/0PHAn\nhQwGRfi+LiBNZ4YBiuOQcsgU1QKBgQC3GOdw4FA5WxpDRzN4VXlcXMgKTCJfofzc\n+fZVv64HKHxJ7GUQDbjoEjcmAuyStFoB6MCNtljVv1k7ghAPl0de+uweYsviTeW2\nb4+LMjNgBXtYAOSRJwlz1C7RMv4TR8n+PxkE8a5ACE7KDRhnoHQ8INkLrgN1/xUD\nGgPmuJLzgwKBgA+xXHNec+T4TWgsjuXf7iWMu9vTSFobQEc/a7cCCdVb6SYAkOJg\nIGNM7tugOm29zlVN/dtph1e6yjbYAYg/s0HlMGLmCSCAy/kjfRRpxaWaAl3av/3R\n93P8ZRsGLsbRceeA397J+G7ESJXJkDIO1/xG2nZqvjJe0r6ccYyfBB5hAoGALUu+\n7eDAWAAetigmJo3WsS/O2SVOl6QGX1CDTWFXFzQ/0zXlJpyOcvMWq5OrnPt7DN/c\nyvIidfaZh6w8du7w3CFtSnMch0bIHmLo+wGXThjtE69c3D/xCbOjRWO9EV4ALaLE\neMGlXvfvLfMhZ3l9Z+aka0xIVOee9h54epqM2CsCgYBmIQT5hGBZ+eVIBVi/vsO5\nU22Zy/uCEyuO6NuBJy0KEJxURSUj4/lQOvRdL9k5CLvrtuwqQfX8yovjmd5NtfWY\n7YHef9QEVYimYzMdKyQgcPGNhQRC+UjkpsWTb+tIqdQW/64fwH+RibbvnlWeB87Z\nqLLfU8CkrhWuci73R9n30A==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-gd5op@productcrudapp.iam.gserviceaccount.com",
-  "client_id": "115516108563404963842",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gd5op%40productcrudapp.iam.gserviceaccount.com"
-}
+//firebase.google.com/docs/storage/admin/start
+https: var serviceAccount = {
+  type: "service_account",
+  project_id: "productcrudapp",
+  private_key_id: "3d8bfb1bc1b42217c4bff649e90cf2d8f55f4354",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCOHQnpfxU6HwIY\nH5DEjQwHZSxJm03DdgFTLD0Ns6wF7XOdpTv3UFuLc//LgToANg6zSgk44Ha3URxz\nXPrZA6/qVaA5nbgK6/LTiaQq5gZu60H9YsRaMqQutt/O5vvHn12cFhWog6V7EFIB\nNG+64b3kwEO/1aghgRYfGV103tzyjncnxJTUkCePcetvK9qjpWsY+tybRuNX0WkO\nzdxrCakdL80HwL73mj73c0AC9CH5zDq6mzrV+DlN8TN6m/CJegky1z8RLsXg0te0\nKw+/JElKAjpur+2HjRzKxtj2K2v23ewRkZ9QtDV5xwJBg2dMR/ycljCl6WLn+E+Z\nC1Ghotf/AgMBAAECggEAA46th1dDiENrcYAkxw7CzNKXA7asAScMjHDwjE3Bkhpq\nCPmwYRX1XK3IKudutL+lqY5CYdaYkikpHagnrVP+zM5c5uRQTZGvb+Ujo2FDqvYf\nnkChKqLW4kZ4nrzAc7RFCxmwbBnzg/uaGeaen150OuwnJf+qqEALdjbT1I+Zl0a5\nHTpKYm1IXzQkGW9B3aXylnGmR6cKWoIlmceGXG6DFDDzXgQqJCxsNQLftkT0Nq+V\nYw4ucP0oqJrkzOKsk5IZwWOeAkzC21v/Jux+nZ0w3FgyDFAIQsN/oPf0u4xAIuRH\nHWmyaZ27gh7fQK9aRso8Golyiz1zqzDTJf5Zwxk0FQKBgQDGsqc1KeejOX1zoi4E\nsKszlHJggGeJefwqt70NvMb+TkTxDJs/wNQ/92bnBwagQkdf93ObErucZWNlAZnt\n3CrPwAOQvO9POsfpBj4dnl7uLYAw67bULV4RmZMpUAPO8pU3IsnGt/L6sZ/0PHAn\nhQwGRfi+LiBNZ4YBiuOQcsgU1QKBgQC3GOdw4FA5WxpDRzN4VXlcXMgKTCJfofzc\n+fZVv64HKHxJ7GUQDbjoEjcmAuyStFoB6MCNtljVv1k7ghAPl0de+uweYsviTeW2\nb4+LMjNgBXtYAOSRJwlz1C7RMv4TR8n+PxkE8a5ACE7KDRhnoHQ8INkLrgN1/xUD\nGgPmuJLzgwKBgA+xXHNec+T4TWgsjuXf7iWMu9vTSFobQEc/a7cCCdVb6SYAkOJg\nIGNM7tugOm29zlVN/dtph1e6yjbYAYg/s0HlMGLmCSCAy/kjfRRpxaWaAl3av/3R\n93P8ZRsGLsbRceeA397J+G7ESJXJkDIO1/xG2nZqvjJe0r6ccYyfBB5hAoGALUu+\n7eDAWAAetigmJo3WsS/O2SVOl6QGX1CDTWFXFzQ/0zXlJpyOcvMWq5OrnPt7DN/c\nyvIidfaZh6w8du7w3CFtSnMch0bIHmLo+wGXThjtE69c3D/xCbOjRWO9EV4ALaLE\neMGlXvfvLfMhZ3l9Z+aka0xIVOee9h54epqM2CsCgYBmIQT5hGBZ+eVIBVi/vsO5\nU22Zy/uCEyuO6NuBJy0KEJxURSUj4/lQOvRdL9k5CLvrtuwqQfX8yovjmd5NtfWY\n7YHef9QEVYimYzMdKyQgcPGNhQRC+UjkpsWTb+tIqdQW/64fwH+RibbvnlWeB87Z\nqLLfU8CkrhWuci73R9n30A==\n-----END PRIVATE KEY-----\n",
+  client_email:
+    "firebase-adminsdk-gd5op@productcrudapp.iam.gserviceaccount.com",
+  client_id: "115516108563404963842",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gd5op%40productcrudapp.iam.gserviceaccount.com",
+};
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL:  "https://productcrudapp.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://productcrudapp.firebaseio.com",
 });
 const bucket = admin.storage().bucket("gs://productcrudapp.appspot.com");
 
-
-
 //==============================================
-import multer from 'multer';
-const storageConfig = multer.diskStorage({ // https://www.npmjs.com/package/multer#diskstorage
-    destination: './uploads/',
-    filename: function (req, file, cb) {
-
-        console.log("mul-file: ", file);
-        cb(null, `${new Date().getTime()}-${file.originalname}`)
-    }
-})
-var upload = multer({ storage: storageConfig })
+import multer from "multer";
+const storageConfig = multer.diskStorage({
+  // https://www.npmjs.com/package/multer#diskstorage
+  destination: "./uploads/",
+  filename: function (req, file, cb) {
+    console.log("mul-file: ", file);
+    cb(null, `${new Date().getTime()}-${file.originalname}`);
+  },
+});
+var upload = multer({ storage: storageConfig });
 
 //==============================================
 
-
-
-
-
-
-const app = express()
-const port = process.env.PORT || 5000
-app.use(express.json())
-app.use(cors(
-  {
-    origin : ['http://localhost:3000' ,"https://isnt-aqsay-faisal34-gmail-com-awesome-3603d.netlify.app", "*"],
-    credentials : true
-  }
-))
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://isnt-aqsay-faisal34-gmail-com-awesome-3603d.netlify.app",
+      "*",
+    ],
+    credentials: true,
+  })
+);
 
 let productSchema = new mongoose.Schema({
   name: { type: String },
   description: { type: String },
   price: { type: Number },
   productimage: { type: String, required: true },
-})
+});
 
-let productModel = mongoose.model("product", productSchema)
+let productModel = mongoose.model("product", productSchema);
 
+app.post("/product", upload.any(), async (req, res) => {
+  console.log("product received", req.body);
 
-
-
-
-app.post('/product', upload.any(), async(req, res) => {
-  console.log("product received", req.body)
-
-  console.log("files", req.files[0])
-
-
-
+  console.log("files", req.files[0]);
 
   bucket.upload(
-      req.files[0].path,
-      {
-          destination: `productPhotos/${req.files[0].filename}`, // give destination name if you want to give a certain name to file in bucket, include date to make name unique otherwise it will replace previous file with the same name
-      },
-      function (err, file, apiResponse ) {
-          if (!err) {
-              // console.log("api resp: ", apiResponse);
+    req.files[0].path,
+    {
+      destination: `productPhotos/${req.files[0].filename}`, // give destination name if you want to give a certain name to file in bucket, include date to make name unique otherwise it will replace previous file with the same name
+    },
+    function (err, file, apiResponse) {
+      if (!err) {
+        // console.log("api resp: ", apiResponse);
 
-              // https://googleapis.dev/nodejs/storage/latest/Bucket.html#getSignedUrl
-              file.getSignedUrl({
-                  action: 'read',
-                  expires: '03-09-2491'
-              }).then(async (urlData, err) => {
-                  if (!err) {
-                      console.log("public downloadable url: ", urlData[0]) // this is public downloadable url 
+        // https://googleapis.dev/nodejs/storage/latest/Bucket.html#getSignedUrl
+        file
+          .getSignedUrl({
+            action: "read",
+            expires: "03-09-2491",
+          })
+          .then(async (urlData, err) => {
+            if (!err) {
+              console.log("public downloadable url: ", urlData[0]); // this is public downloadable url
 
-                      // delete file from folder before sending response back to client (optional but recommended)
-                      // optional because it is gonna delete automatically sooner or later
-                      // recommended because you may run out of space if you dont do so, and if your files are sensitive it is simply not safe in server folder
-                      try {
-                          fs.unlinkSync(req.files[0].path)
-                          //file removed
-                      } catch (err) {
-                          console.error(err)
-                      }
+              // delete file from folder before sending response back to client (optional but recommended)
+              // optional because it is gonna delete automatically sooner or later
+              // recommended because you may run out of space if you dont do so, and if your files are sensitive it is simply not safe in server folder
+              try {
+                fs.unlinkSync(req.files[0].path);
+                //file removed
+              } catch (err) {
+                console.error(err);
+              }
 
-
-                      let newProduct = new productModel({
-                          name: req.body.name,
-                          description: req.body.description,
-                          price: req.body.price,
-                         productimage : urlData[0],
-                           })
-                        try {
-                          let response = await newProduct.save()
-                          console.log("product added", response)
-                          console.log(urlData[0])
-                          res.send({
-                            message: "product added",
-                            data: {
-                              name: req.body.name,
-                              description: req.body.description,
-                              price : req.body.price,
-                              productimage: urlData[0],
-                               }
-                          })
-                      
-                        }
-                      
-                        catch (error) {
-                          console.log("failed to add product" , error)
-                          res.status(500).send({
-                            message: "failed to add product"
-                          })
-                        }
-                  }
-              })
-          } else {
-              console.log("err: ", err)
-              res.status(500).send();
-          }
-      });
-
-  })
-
-
-  app.get("/products", (req, res) => {
-      productModel.find({}, (err, result) => {
-          if (err) {
-              res.send({
-                  message: "error in getting all products"
-              })
-              console.log(err, "error in db")
-              return;
-          } else {
-              res.send({
-                  message: "got all products",
-                  data: result
-              })
-          }
+              let newProduct = new productModel({
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                productimage: urlData[0],
+              });
+              try {
+                let response = await newProduct.save();
+                console.log("product added", response);
+                console.log(urlData[0]);
+                res.send({
+                  message: "product added",
+                  data: {
+                    name: req.body.name,
+                    description: req.body.description,
+                    price: req.body.price,
+                    productimage: urlData[0],
+                  },
+                });
+              } catch (error) {
+                console.log("failed to add product", error);
+                res.status(500).send({
+                  message: "failed to add product",
+                });
+              }
+            }
+          });
+      } else {
+        console.log("err: ", err);
+        res.status(500).send();
       }
-  
-  
-  
-  
-  
-      )
-  })
+    }
+  );
+});
 
-
- 
-  
-  
- 
-
+app.get("/products", (req, res) => {
+  productModel.find({}, (err, result) => {
+    if (err) {
+      res.send({
+        message: "error in getting all products",
+      });
+      console.log(err, "error in db");
+      return;
+    } else {
+      res.send({
+        message: "got all products",
+        data: result,
+      });
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-
-const dbURI = process.env.MONGODBURI || 'mongodb+srv://abc:abc@cluster0.iunhwh0.mongodb.net/Productcrud?retryWrites=true&w=majority';
+const dbURI =
+  process.env.MONGODBURI ||
+  "mongodb+srv://abc:abc@cluster0.iunhwh0.mongodb.net/Productcrud?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
 ////////////////mongodb connected disconnected events///////////////////////////////////////////////
-mongoose.connection.on('connected', function () {//connected
+mongoose.connection.on("connected", function () {
+  //connected
   console.log("Mongoose is connected");
 });
 
-mongoose.connection.on('disconnected', function () {//disconnected
+mongoose.connection.on("disconnected", function () {
+  //disconnected
   console.log("Mongoose is disconnected");
   process.exit(1);
 });
 
-mongoose.connection.on('error', function (err) {//any error
-  console.log('Mongoose connection error: ', err);
+mongoose.connection.on("error", function (err) {
+  //any error
+  console.log("Mongoose connection error: ", err);
   process.exit(1);
 });
 
-process.on('SIGINT', function () {/////this function will run jst before app is closing
+process.on("SIGINT", function () {
+  /////this function will run jst before app is closing
   console.log("app is terminating");
   mongoose.connection.close(function () {
-    console.log('Mongoose default connection closed');
+    console.log("Mongoose default connection closed");
     process.exit(0);
   });
 });
--- web/src/App.js
+++ web/src/App.js
@@ -1,32 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 import { useEffect, useState } from "react";
-import './App.css';
-
-
+import "./App.css";
 
 function Product() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-  const [price, setPrice] = useState("")
-
-  const [users, setUsers] = useState([])
-  const [toggleRefresh, setToggleRefresh] = useState(true)
+  const [users, setUsers] = useState([]);
+  const [toggleRefresh, setToggleRefresh] = useState(true);
 
   useEffect(() => {
-
     let getAllUsers = async () => {
-       let response = await axios.get('https://productcrudapp.herokuapp.com/products');
-     
-      setUsers(response.data.data)
-    }
+      let response = await axios.get(
+        "https://productcrudapp.herokuapp.com/products"
+      );
+
+      setUsers(response.data.data);
+    };
     getAllUsers();
-
-  }, [toggleRefresh])
-
-
-
-
+  }, [toggleRefresh]);
 
   const producthandler = async (e) => {
     e.preventDefault();
@@ -34,89 +27,103 @@ function Product() {
     var productimage = document.getElementById("productimage");
     console.log("fileInput: ", productimage.files); // local url
 
-    
-
     let formData = new FormData();
     // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append#syntax
-
 
     formData.append("name", name); // this is how you add some text data along with file
     formData.append("description", description); // this is how you add some text data along with file
     formData.append("price", price); // this is how you add some text data along with file
     formData.append("productimage", productimage.files[0]); // file input is for browser only, use fs to read file in nodejs client
-    
 
-  axios({
-      method: 'post',
+    axios({
+      method: "post",
       url: "https://productcrudapp.herokuapp.com/product",
       data: formData,
-      headers: { 'Content-Type': 'multipart/form-data' },
-      withCredentials: true
+      headers: { "Content-Type": "multipart/form-data" },
+      withCredentials: true,
     })
-      .then(res => {
+      .then((res) => {
         console.log(`upload Success` + res.data);
-        setToggleRefresh(!toggleRefresh)
+        setToggleRefresh(!toggleRefresh);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }
-
+      });
+  };
 
   return (
     <div>
       <div className="form">
-      <form onSubmit={producthandler}>
-        <h1>PRODUCT FORM</h1>
-
-        Name: <input name="name" type="text" id='name' onChange={(e) => { setName(e.target.value) }} />
-        <br />
-        Description: <input name="description" type="text"  id='description' onChange={(e) => { setDescription(e.target.value) }} />
-        <br />
-        Price: <input name="price" type="Number"  id='price' onChange={(e) => { setPrice(e.target.value) }} />
-        <br />
-
-        Product Image: <input type="file" id="productimage" accept='image/*'
-          onChange={() => {
-            ////// to display imager instantly on screen
-            var productimage = document.getElementById("productimage");
-            var url = URL.createObjectURL(productimage.files[0])
-            console.log("url: ", url);
-            document.getElementById("img").innerHTML = `<img width="200px" src="${url}" alt="" id="img"> `
-          }} />
-
-         
-        <div id="img" ></div>
-     
-        <br />
-        <button type='submit'>Add Product</button>
-
-      </form>
+        <form onSubmit={producthandler}>
+          <h1>PRODUCT FORM</h1>
+          Name:{" "}
+          <input
+            name="name"
+            type="text"
+            id="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <br />
+          Description:{" "}
+          <input
+            name="description"
+            type="text"
+            id="description"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          <br />
+          Price:{" "}
+          <input
+            name="price"
+            type="Number"
+            id="price"
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
+          <br />
+          Product Image:{" "}
+          <input
+            type="file"
+            id="productimage"
+            accept="image/*"
+            onChange={() => {
+              ////// to display imager instantly on screen
+              var productimage = document.getElementById("productimage");
+              var url = URL.createObjectURL(productimage.files[0]);
+              console.log("url: ", url);
+              document.getElementById(
+                "img"
+              ).innerHTML = `<img width="200px" src="${url}" alt="" id="img"> `;
+            }}
+          />
+          <div id="img"></div>
+          <br />
+          <button type="submit">Add Product</button>
+        </form>
       </div>
-
 
       <h1>Products List: </h1>
 
-      <div className='productlist'>
-        {users.map(eachUser => (
+      <div className="productlist">
+        {users.map((eachUser) => (
           <div key={eachUser.id}>
-            <div className='product'>
-            <img width="150px" src={eachUser.productimage} alt="" />
-            <h4>{eachUser.name}</h4>
-            <p className='description'>{eachUser.description}</p>
-            <p ><span className='price'>{eachUser.price}</span><span>PKR</span></p>
-         
-           
+            <div className="product">
+              <img width="150px" src={eachUser.productimage} alt="" />
+              <h2 className="name">{eachUser.name}</h2>
+              <p className="description">{eachUser.description}</p>
+              <p>
+                <span className="price">{eachUser.price}</span>
+                <span>PKR</span>
+              </p>
             </div>
           </div>
         ))}
       </div>
-
-
-
-
-
-     
     </div>
   );
 }
